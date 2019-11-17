b92db58ddfc28cad2f977223c3e4e1c5342065a4
hw <hussainweb@gmail.com>
2019-11-17 12:03:32 -0500

    Add a quick clean-up script.

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -ex
+
+docker rmi jakzal/phpqa:php7.1
+docker rmi jakzal/phpqa:php7.2
+docker rmi jakzal/phpqa:php7.3
+
+docker rmi hussainweb/drupalqa:php7.1
+docker rmi hussainweb/drupalqa:php7.2
+docker rmi hussainweb/drupalqa:php7.3
+docker rmi hussainweb/drupalqa:latest
