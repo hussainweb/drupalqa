bfb4edea1717f928e5d81e1971946effe00c8b9e
hw <hussainweb@gmail.com>
2019-12-17 12:20:40 -0500

    Add php7.4 image names to clean.sh

--- clean.sh
+++ clean.sh
@@ -5,8 +5,10 @@ set -ex
 docker rmi jakzal/phpqa:php7.1
 docker rmi jakzal/phpqa:php7.2
 docker rmi jakzal/phpqa:php7.3
+docker rmi jakzal/phpqa:php7.4
 
 docker rmi hussainweb/drupalqa:php7.1
 docker rmi hussainweb/drupalqa:php7.2
 docker rmi hussainweb/drupalqa:php7.3
+docker rmi hussainweb/drupalqa:php7.4
 docker rmi hussainweb/drupalqa:latest
