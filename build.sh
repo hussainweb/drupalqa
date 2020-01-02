0da9e0b9f9884bc902213d5e734822c92f88a6e9
hw <hussainweb@gmail.com>
2020-01-02 16:20:23 -0500

    Remove extra Dockerfiles with a single one using the ARG syntax.

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ docker pull jakzal/phpqa:php7.2
 docker pull jakzal/phpqa:php7.3
 docker pull jakzal/phpqa:php7.4
 
-docker build -t hussainweb/drupalqa:php7.1 ${dir}/7.1/debian/
-docker build -t hussainweb/drupalqa:php7.2 ${dir}/7.2/debian/
-docker build -t hussainweb/drupalqa:php7.3 ${dir}/7.3/debian/
-docker build -t hussainweb/drupalqa:php7.4 ${dir}/7.4/debian/
-docker build -t hussainweb/drupalqa:latest ${dir}
+docker build -t hussainweb/drupalqa:php7.1 --build-arg PHP_VERSION=php7.1 ${dir}/7.3/debian/
+docker build -t hussainweb/drupalqa:php7.2 --build-arg PHP_VERSION=php7.2 ${dir}/7.3/debian/
+docker build -t hussainweb/drupalqa:php7.3 --build-arg PHP_VERSION=php7.3 ${dir}/7.3/debian/
+docker build -t hussainweb/drupalqa:php7.4 --build-arg PHP_VERSION=php7.4 ${dir}/7.4/debian/
+docker tag hussainweb/drupalqa:php7.4 hussainweb/drupalqa:latest
 
 docker push hussainweb/drupalqa:php7.1
 docker push hussainweb/drupalqa:php7.2
