3cc18bc509d5b79c841ebfbf8c11859807a88862
hw <hussainweb@gmail.com>
2020-04-02 17:17:01 -0400

    Remove PHP 7.1 image references from the clean-up script.

--- clean.sh
+++ clean.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-set -ex
-
-docker rmi jakzal/phpqa:php7.1
 docker rmi jakzal/phpqa:php7.2
 docker rmi jakzal/phpqa:php7.3
 docker rmi jakzal/phpqa:php7.4
 
-docker rmi hussainweb/drupalqa:php7.1
 docker rmi hussainweb/drupalqa:php7.2
 docker rmi hussainweb/drupalqa:php7.3
 docker rmi hussainweb/drupalqa:php7.4
