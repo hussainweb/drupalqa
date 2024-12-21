15decedb6ca5e81485e9613d7a8f364f3d9763c6
hw <hussainweb@gmail.com>
2024-12-21 10:58:55 -0500

    feat: add support for PHP 8.4

--- clean.sh
+++ clean.sh
@@ -4,9 +4,11 @@ docker rmi jakzal/phpqa:php8.0
 docker rmi jakzal/phpqa:php8.1
 docker rmi jakzal/phpqa:php8.2
 docker rmi jakzal/phpqa:php8.3
+docker rmi jakzal/phpqa:php8.4
 
 docker rmi hussainweb/drupalqa:php8.0
 docker rmi hussainweb/drupalqa:php8.1
 docker rmi hussainweb/drupalqa:php8.2
 docker rmi hussainweb/drupalqa:php8.3
+docker rmi hussainweb/drupalqa:php8.4
 docker rmi hussainweb/drupalqa:latest
